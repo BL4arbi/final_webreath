971eb4b93e9d77c87e2e74092eb45142943acd3b
lboutouil <lboutouil@icloud.com>
2023-08-23 12:30:21 +0200

    ajout des migrations

---
+++ migrations/Version20230823102720.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230823102720 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE module (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(40) NOT NULL, detail VARCHAR(255) DEFAULT NULL, statut TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE module_data (id INT AUTO_INCREMENT NOT NULL, module_id INT NOT NULL, data VARCHAR(255) NOT NULL, time DATETIME NOT NULL, type VARCHAR(50) NOT NULL, INDEX IDX_17CFB66FAFC2B591 (module_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE module_data ADD CONSTRAINT FK_17CFB66FAFC2B591 FOREIGN KEY (module_id) REFERENCES module (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE module_data DROP FOREIGN KEY FK_17CFB66FAFC2B591');
+        $this->addSql('DROP TABLE module');
+        $this->addSql('DROP TABLE module_data');
+    }
+}
